3feeb7863d1daa35a648095fee7de616e8419a12
michaelvlysdal <michael@vestergard.dk>
2018-03-26 14:42:08 +0200

    Added a few things to the heroku express stuff

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`started on port ${port}`);
+});
