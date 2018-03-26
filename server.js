c701e3c292a7002465e5724147d8351f8e52ee81
michaelvlysdal <michael@vestergard.dk>
2018-03-26 15:08:46 +0200

    made express js default serve index html, as we are using html5 navigation

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
+// little heroku express webserver
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(`${__dirname}/dist`));
+
+//default to index.html
+app.use((req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 
 // Start the app
 const port = process.env.PORT || 8080;
